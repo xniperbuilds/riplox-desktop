c8e972e039fa4c383dba4eada0d22df2bf2ef5c9
XniperBuilds <xniperbuilds@users.noreply.github.com>
2026-09-14 06:01:56 +0500

    Package listings point at 1.7.0

--- packaging/chocolatey/tools/chocolateyinstall.ps1
+++ packaging/chocolatey/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
     packageName    = 'riplox'
     fileType       = 'exe'
-    url64bit       = 'https://github.com/xniperbuilds/riplox-desktop/releases/download/v1.6.1/Riplox_Setup_v1.6.1.exe'
-    checksum64     = '4EF94E5BCADD244D88E1BDF3F361655AAC7EB9B9DC516BBBBDB1A4493FCD848F'
+    url64bit       = 'https://github.com/xniperbuilds/riplox-desktop/releases/download/v1.7.0/Riplox_Setup_v1.7.0.exe'
+    checksum64     = 'BEE8BCE6332FA3D39A5590F2A20F21EFC6C7C1A2C8B4EC8398C6BA036FF28E03'
     checksumType64 = 'sha256'
     # Inno Setup 6. installer.iss sets PrivilegesRequired=lowest, so this lands
     # in the user's profile without an admin prompt.
